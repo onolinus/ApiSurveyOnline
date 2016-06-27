9051e902667fb321723db5cc2b6e249f2e50a9b0
Harry Sitohang <harry@olx.co.id>
2016-06-27 14:41:50 +0700

    add route resources middleware group apisurveylitbang

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['apisurveylitbang']], function () {
+    Route::resource('user', 'UserController', ['only' => [
+        'store', 'destroy', 'update', 'show'
+    ]]);
 });
