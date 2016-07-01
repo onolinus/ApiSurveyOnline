574f0fba99066e47717f432cefca1d46044a0142
Harry Sitohang <harry@olx.co.id>
2016-07-01 08:55:19 +0700

    refactor quest2 & quest3

---
+++ app/Libraries/Questions/Question2.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\Libraries\Questions;
+
+use function app\Helper\Questions\Chapter\getChaptersData;
+
+class Question2 extends AbstractQuestion{
+
+    CONST NUMBER = 2;
+    CONST CHAPTER_NUMBER = 2;
+
+    private $jumlah;
+
+    public function __construct($jumlah)
+    {
+        $this->setJumlah($jumlah);
+    }
+
+    /**
+     * @param mixed $jumlah
+     */
+    public function setJumlah($jumlah)
+    {
+        $this->jumlah = $jumlah;
+        $this->postSetter();
+        return $this;
+    }
+
+    public function getRules()
+    {
+        return [
+            'jumlah' => 'required|numeric',
+        ];
+    }
+
+    protected function getAnswer()
+    {
+        return [
+            'jumlah' => $this->jumlah
+        ];
+    }
+}
---
+++ app/Libraries/Questions/Question3.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\Libraries\Questions;
+
+use function app\Helper\Questions\Chapter\getChaptersData;
+
+class Question3 extends AbstractQuestion{
+
+    CONST NUMBER = 3;
+    CONST CHAPTER_NUMBER = 2;
+
+    private $dipa, $dana_lainnya;
+
+    private $dipa_keys = ['dana_pemerintah', 'PNBP', 'PHLN'];
+    private $dana_lainnya_keys = ['perusahaan_swasta', 'instansi_pemerintah', 'swasta_non_profit', 'luar_negeri'];
+
+    public function __construct($dipa, $dana_lainnya)
+    {
+        $this->setDipa($dipa)->setDanaLainnya($dana_lainnya);
+    }
+
+    public function getAnswer()
+    {
+        return [
+            'dipa' => $this->dipa,
+            'dana_lainnya' => $this->dana_lainnya
+        ];
+    }
+
+    /**
+     * @param array $dipa
+     */
+    public function setDipa($dipa)
+    {
+        if(is_array($dipa)){
+            foreach($dipa as $key => $value){
+                if(in_array($key, $this->dipa_keys)){
+                    $this->dipa[$key] = $value;
+                }
+            }
+        }
+
+        $this->postSetter();
+        return $this;
+    }
+
+    /**
+     * @param mixed $dana_lainnya
+     */
+    public function setDanaLainnya($dana_lainnya)
+    {
+        if(is_array($dana_lainnya)){
+            foreach($dana_lainnya as $key => $value){
+                if(in_array($key, $this->dana_lainnya_keys)){
+                    $this->dana_lainnya[$key] = $value;
+                }
+            }
+        }
+
+        $this->postSetter();
+        return $this;
+    }
+
+    public function getRules()
+    {
+        // TODO: Implement getRules() method.
+    }
+}
