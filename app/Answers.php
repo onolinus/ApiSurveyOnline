4956fac26c67c4649571a6a9e1275ef43d97d38f
Harry Sitohang <harry@olx.co.id>
2016-07-05 16:30:41 +0700

    add new model answer + migration, rename userModel => Users

---
+++ app/Answers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answers extends Model
+{
+    protected $table = 'answers';
+
+    protected $primaryKey = 'id';
+
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    protected $connection = 'mysql';
+}
--- app/Users.php
+++ app/Users.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserModel extends Model
+class Users extends Model
 {
     protected $table = 'users';
 
