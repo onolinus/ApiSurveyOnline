d0b4a7540f51e6b5e88c90a63f830b541fb004f0
Harry Sitohang <harry@olx.co.id>
2016-07-12 17:26:58 +0700

    extract libraries warmup

--- app/Console/Commands/WarmUp.php
+++ app/Console/Commands/WarmUp.php
@@ -2,14 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\ResearchFields;
-use App\SocioEconomics;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Cache;
-use app\Libraries\SurveyCacheKey;
-use app\Libraries\ResearchFields as LibResearchFields;
-use app\Libraries\SocioEconomics as LibSocioEconomics;
-
+use app\Libraries\WarmUp as LibrariesWarmUp;
 
 class WarmUp extends Command
 {
@@ -44,104 +38,6 @@ class WarmUp extends Command
      */
     public function handle()
     {
-        $build_number = SurveyCacheKey::getInstance()->setBuildNumber(true);
-        $reset_option = boolval($this->option('reset'));
-        $keepoldcache_option = boolval($this->option('keepoldcache'));
-
-        $cacheGenerated = [];
-
-
-        if($this->argument('cachename') === 'all' || $this->argument('cachename') === LibResearchFields::CACHE_PREFIKS){
-            $this->getResearchFields($reset_option, $keepoldcache_option);
-            $cacheGenerated[] = LibResearchFields::CACHE_PREFIKS;
-        }
-
-        if($this->argument('cachename') === 'all' || $this->argument('cachename') === LibSocioEconomics::CACHE_PREFIKS) {
-            $this->getSocioEconomics($reset_option, $keepoldcache_option);
-            $cacheGenerated[] = LibSocioEconomics::CACHE_PREFIKS;
-        }
-
-
-        if(empty($cacheGenerated)){
-            throw new \Exception("Invalid argument parameter, must be : [all]|researchfields|socioeconomics [--reset=0|1] [--keepoldcache=1|0]");
-        }
-
-        $this->info(sprintf('Generate Cache build-number:%d for "%s"', $build_number, implode(', ', $cacheGenerated)));
+        $this->info(LibrariesWarmUp::getInstance()->handle($this->input->getArguments(), $this->input->getOptions()));
     }
-
-    protected function getResearchFields($reset = false, $keepoldcache_option = true)
-    {
-        $key = SurveyCacheKey::getInstance()->generateCacheKey(LibResearchFields::CACHE_PREFIKS);
-        $oldkey = SurveyCacheKey::getInstance()->generateOldCacheKey(LibResearchFields::CACHE_PREFIKS);
-
-        if($keepoldcache_option === false && $oldkey){
-            Cache::pull($oldkey); // delete old cache key
-        }
-
-        if ($reset === false && $research_fields = Cache::get($key)) {
-            return $research_fields;
-        } else {
-            $research_fields = $this->getCustomizeResearchFields();
-
-            Cache::forever($key, $research_fields);
-
-            return $research_fields;
-        }
-    }
-
-    protected function getSocioEconomics($reset = false, $keepoldcache_option = true)
-    {
-        $key = SurveyCacheKey::getInstance()->generateCacheKey(LibSocioEconomics::CACHE_PREFIKS);
-        $oldkey = SurveyCacheKey::getInstance()->generateOldCacheKey(LibSocioEconomics::CACHE_PREFIKS);
-
-        if($keepoldcache_option === false && $oldkey){
-            Cache::pull($oldkey); // delete old cache key
-        }
-
-        if ($reset === false && $socio_economics = Cache::get($key)) {
-
-            return $socio_economics;
-        } else {
-            $socio_economics = $this->getCustomizeSocioEconomics();
-
-            Cache::forever($key, $socio_economics);
-
-            return $socio_economics;
-        }
-    }
-
-    private function getCustomizeSocioEconomics(){
-        $result = [];
-
-        // get all data `socio_economics` from database
-        $socio_economics = SocioEconomics::all();
-        foreach($socio_economics as $socio_economic){
-            $result[$socio_economic->code] = [
-                'code' => $socio_economic->code,
-                'division' => $socio_economic->division,
-                'division_number' => $socio_economic->division_number,
-                'group' => $socio_economic->group,
-            ];
-        }
-
-        return $result;
-    }
-
-    private function getCustomizeResearchFields(){
-        $result = [];
-
-        // get all data `research_fields` from database
-        $research_fields = ResearchFields::all();
-        foreach($research_fields as $research_field){
-            $result[$research_field->code] = [
-                'code' => $research_field->code,
-                'subject' => $research_field->subject,
-                'area' => $research_field->area,
-                'sub_area' => $research_field->sub_area,
-            ];
-        }
-
-        return $result;
-    }
-
 }
---
+++ app/Libraries/Warmup.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace app\Libraries;
+
+use Illuminate\Support\Facades\Cache;
+
+use App\ResearchFields as ModelResearchFields;
+use App\SocioEconomics as ModelSocioEconomics;
+use app\Libraries\ResearchFields as LibResearchFields;
+use app\Libraries\SocioEconomics as LibSocioEconomics;
+
+class WarmUp{
+
+    /**
+     * @var self
+     */
+    private static $instance;
+
+    /**
+     * @var SurveyCacheKey
+     */
+    private $surveyCacheKey;
+
+
+    /**
+     * @var ResearchFields
+     */
+    private $libResearchFields;
+
+    /**
+     * @var SocioEconomics
+     */
+    private $libSocioEconomics;
+
+    public function __construct(SurveyCacheKey $surveyCacheKey = null)
+    {
+        $this->surveyCacheKey = is_null($surveyCacheKey) ? SurveyCacheKey::getInstance() : $surveyCacheKey;
+    }
+
+    public static function getInstance(SurveyCacheKey $surveyCacheKey = null){
+        if(is_null(self::$instance)){
+            self::$instance = new self($surveyCacheKey);
+        }
+
+        return self::$instance;
+    }
+
+
+    public function handle($arguments, $options)
+    {
+        $build_number = $this->surveyCacheKey->setBuildNumber(true);
+
+        $cacheGenerated = [];
+
+
+        if($arguments['cachename'] === 'all' || $arguments['cachename'] === LibResearchFields::CACHE_PREFIKS){
+            $this->getResearchFields($options['reset'], $options['keepoldcache']);
+            $cacheGenerated[] = LibResearchFields::CACHE_PREFIKS;
+        }
+
+        if($arguments['cachename'] === 'all' || $arguments['cachename'] === LibSocioEconomics::CACHE_PREFIKS) {
+            $this->getSocioEconomics($options['reset'], $options['keepoldcache']);
+            $cacheGenerated[] = LibSocioEconomics::CACHE_PREFIKS;
+        }
+
+
+        if(empty($cacheGenerated)){
+            throw new \Exception("Invalid argument parameter, must be : [all]|researchfields|socioeconomics [--reset=0|1] [--keepoldcache=1|0]");
+        }
+
+        return sprintf('Generate Cache build-number:%d for "%s"', $build_number, implode(', ', $cacheGenerated));
+    }
+
+    protected function getResearchFields($reset = false, $keepoldcache_option = true)
+    {
+        $key = $this->surveyCacheKey->generateCacheKey(LibResearchFields::CACHE_PREFIKS);
+        $oldkey = $this->surveyCacheKey->generateOldCacheKey(LibResearchFields::CACHE_PREFIKS);
+
+        if($keepoldcache_option === false && $oldkey){
+            Cache::pull($oldkey); // delete old cache key
+        }
+
+        if ($reset === false && $research_fields = Cache::get($key)) {
+            return $research_fields;
+        } else {
+            $research_fields = $this->getCustomizeResearchFields();
+
+            Cache::forever($key, $research_fields);
+
+            return $research_fields;
+        }
+    }
+
+    protected function getSocioEconomics($reset = false, $keepoldcache_option = true)
+    {
+        $key = $this->surveyCacheKey->generateCacheKey(LibSocioEconomics::CACHE_PREFIKS);
+        $oldkey = $this->surveyCacheKey->generateOldCacheKey(LibSocioEconomics::CACHE_PREFIKS);
+
+        if($keepoldcache_option === false && $oldkey){
+            Cache::pull($oldkey); // delete old cache key
+        }
+
+        if ($reset === false && $socio_economics = Cache::get($key)) {
+
+            return $socio_economics;
+        } else {
+            $socio_economics = $this->getCustomizeSocioEconomics();
+
+            Cache::forever($key, $socio_economics);
+
+            return $socio_economics;
+        }
+    }
+
+    private function getCustomizeSocioEconomics(){
+        $result = [];
+
+        // get all data `socio_economics` from database
+        $socio_economics = ModelSocioEconomics::all();
+        foreach($socio_economics as $socio_economic){
+            $result[$socio_economic->code] = [
+                'code' => $socio_economic->code,
+                'division' => $socio_economic->division,
+                'division_number' => $socio_economic->division_number,
+                'group' => $socio_economic->group,
+            ];
+        }
+
+        return $result;
+    }
+
+    private function getCustomizeResearchFields(){
+        $result = [];
+
+        // get all data `research_fields` from database
+        $research_fields = ModelResearchFields::all();
+        foreach($research_fields as $research_field){
+            $result[$research_field->code] = [
+                'code' => $research_field->code,
+                'subject' => $research_field->subject,
+                'area' => $research_field->area,
+                'sub_area' => $research_field->sub_area,
+            ];
+        }
+
+        return $result;
+    }
+
+}
