872d7445fdf63a12f584f3ca03c31ef215d9251a
Harry Sitohang <harry@olx.co.id>
2016-07-15 09:58:01 +0700

    update model RegistrasiToken set table name n primary key

--- app/RegistrasiToken.php
+++ app/RegistrasiToken.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegistrasiToken extends Model
 {
-    //
+    protected $table = 'registrasi_tokens';
+
+    protected $primaryKey = 'token';
+
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    protected $connection = 'mysql';
+
+    public $incrementing = false;
 }
