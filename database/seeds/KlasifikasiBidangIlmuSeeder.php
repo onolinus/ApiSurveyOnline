7d728cab3914a1cb31b28ddc2b2b27ad90971f9c
Harry Sitohang <harry@olx.co.id>
2016-08-10 09:38:46 +0700

    add new table kodebidangilmu

---
+++ database/seeds/KlasifikasiBidangIlmuSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KlasifikasiBidangIlmuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('klasifikasi_bidang_ilmu')->insert([
+
+        ]);
+    }
+}
