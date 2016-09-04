2dd30fb5e56b1de1c9bb93fdfdbfe856bb0a0806
Harry Sitohang <harry@olx.co.id>
2016-09-04 23:08:13 +0700

    separate precheck condition survey answers number base controller

--- app/Http/Controllers/Survey/BaseController.php
+++ app/Http/Controllers/Survey/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Survey;
 
+use App\AnswersNumber;
 use App\Http\Middleware\AdminPrivilegeMiddleware;
 use App\TraitCacheSurveyData;
 use App\TraitFractalResponse;
@@ -16,30 +17,50 @@ abstract class BaseController extends Controller
 
     use TraitCacheSurveyData;
 
-    public function show($id_answer){
+    /**
+     * @var AnswersNumber
+     */
+    protected $answersNumberModel;
+
+    protected $error_message = null;
+
+    private function preCheckAnswers($id_answer){
         /** @var ModelAnswers $answersModel */
         $answers = ModelAnswers::find($id_answer);
 
         if($answers === null){
-            return $this->response->errorNotFound([trans('validator.notfoundanswersdata', ['answersnumber' => 1])]);
+            $this->error_message = $this->response->errorNotFound([trans('validator.notfoundanswersdata', ['answersnumber' => 1])]);
+            return false;
         }
 
-        $answersNumberModel = $answers->{$this->getModelName()};
+        $this->answersNumberModel = $answers->{$this->getModelName()};
 
-        if($answersNumberModel === null || count($answersNumberModel) === 0){
-            return $this->response->errorNotFound([trans('validator.notfoundanswersdata', ['answersnumber' => $this->getModelName()])]);
+        if($this->answersNumberModel === null || count($this->answersNumberModel) === 0){
+            $this->error_message = $this->response->errorNotFound([trans('validator.notfoundanswersdata', ['answersnumber' => $this->getModelName()])]);
+            return false;
         }
 
         if(intval($answers->validator_id) !== $this->getSessionUserID() && $this->getSessionUserType() !== AdminPrivilegeMiddleware::USER_TYPE_ALLOWED){
-            return $this->response->errorForbidden([trans('validator.forbiddenaccesstoanswersnumber')]);
+            $this->error_message = $this->response->errorForbidden([trans('validator.forbiddenaccesstoanswersnumber')]);
+            return false;
         }
 
-        return $this->response->withItem($answersNumberModel, $this->getTransformers());
+        return true;
+    }
+
+    public function show($id_answer){
+        if($this->preCheckAnswers($id_answer) === false){
+            return $this->error_message;
+        }
+
+        return $this->response->withItem($this->answersNumberModel, $this->getTransformers());
     }
 
 
     public function update($id_answer){
-
+        if($this->preCheckAnswers($id_answer) === false){
+            return $this->error_message;
+        }
     }
 
     public function approve(){
