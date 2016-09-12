5f47b98c534c06a712e710a58b8b6699c37b7d24
Harry Sitohang <harry@olx.co.id>
2016-09-12 23:25:33 +0700

    update answers stats endpoint

--- app/Http/Controllers/Guest/Report/SentAnswersController.php
+++ app/Http/Controllers/Guest/Report/SentAnswersController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Guest\Report;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Middleware\CorrespondentPrivilegeMiddleware;
 use EllipseSynergie\ApiResponse\Laravel\Response;
 use Illuminate\Support\Facades\DB;
 
@@ -16,6 +17,18 @@ class SentAnswersController extends Controller
             ->join('approved_by', 'approved_by.correspondent_id_approved', '=', 'correspondents.user_id')
             ->join('lembaga', 'lembaga.id', '=', 'approved_by.id_lembaga')
             ->groupBy('approved_by.id_lembaga')
+            ->get();
+
+        $result = $result = DB::table('lembaga')
+            ->select('lembaga.*', DB::raw('COUNT(answers.id) as count'))
+            ->leftjoin('approved_by', 'approved_by.id_lembaga', '=', 'lembaga.id')
+            ->leftjoin('correspondents', 'correspondents.user_id', '=', 'approved_by.correspondent_id_approved')
+            ->leftjoin('users', function($join){
+                $join->on('users.id', '=', 'correspondents.user_id')
+                    ->where('users.type', '=', CorrespondentPrivilegeMiddleware::USER_TYPE_ALLOWED);
+            })
+            ->leftjoin('answers', 'answers.id_correspondent', '=', 'correspondents.user_id')
+            ->groupBy('lembaga.id')
             ->get();
 
         return $response->withArray(['data' => $result]);
