45fee00b40736b11929471dac80a5a7bbb08f772
Harry Sitohang <harry@olx.co.id>
2016-09-12 22:15:28 +0700

    update count lembaga correspondent register stats/report endpoint

--- app/Http/Controllers/LembagaController.php
+++ app/Http/Controllers/LembagaController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\CorrespondentPrivilegeMiddleware;
 use App\Transformer\Lembaga;
 use App\TraitFractalResponse;
 use App\Http\Requests;
 
 use App\Lembaga as LembagaModel;
+use Illuminate\Support\Facades\DB;
 
 class LembagaController extends BaseController
 {
@@ -34,8 +36,19 @@ class LembagaController extends BaseController
 
     public function getUserCount()
     {
-        $lembaga = LembagaModel::with('usersCount')->get();
+        $result = $result = DB::table('users')
+            ->select('lembaga.*', DB::raw('COUNT(1) as count'))
+            ->join('correspondents', 'correspondents.user_id', '=', 'users.id')
+            ->join('approved_by', 'approved_by.correspondent_id_approved', '=', 'correspondents.user_id')
+            ->join('lembaga', 'lembaga.id', '=', 'approved_by.id_lembaga')
+            ->where('users.type', CorrespondentPrivilegeMiddleware::USER_TYPE_ALLOWED)
+            ->groupBy('approved_by.id_lembaga')
+            ->get();
 
-        return $this->response->withCollection($lembaga, $this->getTransformer(), null, null);
+        return $this->response->withArray(['data' => $result]);
+
+//        $lembaga = LembagaModel::with('usersCount')->get();
+
+//        return $this->response->withCollection($lembaga, $this->getTransformer(), null, null);
     }
 }
