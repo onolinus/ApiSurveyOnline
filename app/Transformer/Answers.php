c6a04f1d8cf6ca1cef1dfb51bf0feb792084c803
Harry Sitohang <harry@olx.co.id>
2016-09-16 23:20:06 +0700

    add option include `correspondent` in list survey validator

--- app/Transformer/Answers.php
+++ app/Transformer/Answers.php
@@ -2,12 +2,16 @@
 namespace App\Transformer;
 
 use App\Answers as ModelAnswers;
+use Illuminate\Support\Facades\Request;
 use League\Fractal;
 
 class Answers extends Fractal\TransformerAbstract
 {
     public function transform(ModelAnswers $answers)
     {
+        $request = Request::capture();
+        $includes = explode(',', $request->include);
+
         return [
             'id' => $answers->id,
             'status' => $answers->status,
@@ -24,11 +28,35 @@ class Answers extends Fractal\TransformerAbstract
                 'self' => route('user.{user_id}.survey.show', [$answers->id_correspondent])
             ],
             'relationships' => [
-                'correspondent' => [
-                    'links' => [
-                        'related' => route('admin.correspondent.show', [$answers->id_correspondent])
-                    ]
+                'correspondent' => $this->getCorrespondent($includes, $answers),
+            ]
+        ];
+    }
+
+    private function getCorrespondent($includes, ModelAnswers $answers)
+    {
+        $correspondent = $answers->Correspondents;
+
+        if(in_array('correspondent', $includes) && $correspondent !== null){
+            return [
+                'user_id' => $correspondent->user_id,
+                'name' => $correspondent->name,
+                'nip' => $correspondent->nip,
+                'role' => $correspondent->role,
+                'telephone_number' => $correspondent->telephone_number,
+                'handphone_number' => $correspondent->handphone_number,
+                'timestamp' => [
+                    'created' => $correspondent->created_at,
+                    'created_string' => $correspondent->created_at->toDayDateTimeString(),
+                    'updated' => $correspondent->updated_at,
+                    'updated_string' => $correspondent->updated_at->toDayDateTimeString(),
                 ],
+            ];
+        }
+
+        return [
+            'links' => [
+                'related' => route('admin.correspondent.show', [$answers->id_correspondent])
             ]
         ];
     }
