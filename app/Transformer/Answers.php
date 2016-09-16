08e3ab532a9c49b1f07216ee1529cf136f50678a
Harry Sitohang <harry@olx.co.id>
2016-09-16 22:54:10 +0700

    fix boken link create route validator survey list

--- app/Transformer/Answers.php
+++ app/Transformer/Answers.php
@@ -21,7 +21,7 @@ class Answers extends Fractal\TransformerAbstract
                 'updated_string' => $answers->updated_at->toDayDateTimeString(),
             ],
             'links' => [
-                'self' => route('user.{id}.survey.show', [$answers->id_correspondent])
+                'self' => route('user.{user_id}.survey.show', [$answers->id_correspondent])
             ],
             'relationships' => [
                 'correspondent' => [
