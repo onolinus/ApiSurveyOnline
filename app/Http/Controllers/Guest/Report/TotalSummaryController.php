5a04547be356e690c65b584efb3299eb751396b2
Harry Sitohang <harry@olx.co.id>
2016-09-25 20:31:41 +0700

    add transformers in totalsummary

--- app/Http/Controllers/Guest/Report/TotalSummaryController.php
+++ app/Http/Controllers/Guest/Report/TotalSummaryController.php
@@ -6,17 +6,16 @@ use App\Answers;
 use App\Correspondents;
 use App\Http\Controllers\Controller;
 use App\Lembaga;
+use App\Transformer\Report\TotalSummary;
 use EllipseSynergie\ApiResponse\Laravel\Response;
 
 class TotalSummaryController extends Controller
 {
     public function index(Response $response){
-        return $response->withArray([
-            'data' => [
+        return $response->withItem(collect([
                 'lembaga' => Lembaga::count(),
                 'correspondent' => Correspondents::count(),
                 'answers' => Answers::count(),
-            ]
-        ]);
+            ]), new TotalSummary());
     }
 }
