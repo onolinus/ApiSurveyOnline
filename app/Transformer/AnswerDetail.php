d66c2bb9228857edcb85298a04d1597dbde6be5c
Harry Sitohang <harry@olx.co.id>
2016-10-07 21:54:11 +0700

    add tahun in answer detail no 16a

--- app/Transformer/AnswerDetail.php
+++ app/Transformer/AnswerDetail.php
@@ -676,8 +676,8 @@ class AnswerDetail extends Fractal\TransformerAbstract
 
             $detail['answer16a']['data'][] = [
                 'id' => $answers16a->id,
+                'tahun' => $answers16a->tahun,
                 'usulan_paten' => intval($answers16a->usulan_paten),
-                'usulan_patensederhana' => intval($answers16a->usulan_patensederhana),
                 'usulan_patensederhana' => intval($answers16a->usulan_patensederhana),
                 'disetujui_paten' => intval($answers16a->disetujui_paten),
                 'disetujui_patensederhana' => intval($answers16a->disetujui_patensederhana),
@@ -689,7 +689,7 @@ class AnswerDetail extends Fractal\TransformerAbstract
         $detail['answer16a']['links'] = [
             'self' => route("survey.{id_answer}.answers16a.show", $answers16a->id_answer),
             'approve' => route("survey.{id_answer}.answers16a.approve", $answers16a->id_answer),
-                'reject' => route("survey.{id_answer}.answers16a.reject", $answers16a->id_answer),
+            'reject' => route("survey.{id_answer}.answers16a.reject", $answers16a->id_answer),
         ];
 
         return $this;
