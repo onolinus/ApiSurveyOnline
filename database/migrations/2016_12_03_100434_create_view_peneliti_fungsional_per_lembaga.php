90026cbd674f60031f1ffedccfcf4e16e1542ff6
Harry Sitohang <harry@olx.co.id>
2016-12-03 10:09:35 +0700

    add query to create view jumlah_peneliti_fungsional_per_lembaga

--- database/migrations/2016_12_03_100434_create_view_peneliti_fungsional_per_lembaga.php
+++ database/migrations/2016_12_03_100434_create_view_peneliti_fungsional_per_lembaga.php
@@ -12,7 +12,32 @@ class CreateViewPenelitiFungsionalPerLembaga extends Migration
      */
     public function up()
     {
-        //
+        DB::unprepared('CREATE VIEW `jumlah_peneliti_fungsional_per_lembaga` AS
+        SELECT (
+                        SUM(peneliti_fungsional_peneliti_s1_l)
+                        +SUM(peneliti_fungsional_peneliti_s1_p)
+                        +SUM(peneliti_fungsional_peneliti_s2_l)
+                        +SUM(peneliti_fungsional_peneliti_s2_p)
+                        +SUM(peneliti_fungsional_peneliti_s3_l)
+                        +SUM(peneliti_fungsional_peneliti_s3_p)
+                    ) AS `jumlah_peneliti_fungsional`,
+                    (
+                    	SUM(peneliti_fungsional_peneliti_s1_l)
+                        +SUM(peneliti_fungsional_peneliti_s1_p)
+                        +SUM(peneliti_fungsional_peneliti_s2_l)
+                        +SUM(peneliti_fungsional_peneliti_s2_p)
+                        +SUM(peneliti_fungsional_peneliti_s3_l)
+                        +SUM(peneliti_fungsional_peneliti_s3_p)
+                     )/jumlah_peneliti_fungsional_personil_litbang() * 100 AS `percentage`,
+                    `lembaga`.`id` AS `id_lembaga`,
+					`lembaga`.`name` AS `nama_lembaga`,
+					`lembaga`.`type` AS `tipe_lembaga`
+                    FROM `answers9b`
+                    JOIN `answers` ON `answers`.`id` = `answers9b`.`id_answer`
+                    JOIN `correspondents` ON `correspondents`.`user_id` = `answers`.`id_correspondent`
+                    JOIN `approved_by` ON `approved_by`.`correspondent_id_approved` = `correspondents`.`user_id`
+                    JOIN `lembaga` ON `lembaga`.`id` = `approved_by`.`id_lembaga`
+                    GROUP BY `lembaga`.`id`');
     }
 
     /**
@@ -22,6 +47,6 @@ class CreateViewPenelitiFungsionalPerLembaga extends Migration
      */
     public function down()
     {
-        //
+        DB::unprepared('DROP VIEW `jumlah_peneliti_fungsional_per_lembaga`');
     }
 }
